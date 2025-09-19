52ed459b39d42e63bed4afb52e12dc48a82a49f6
Povilas <ttphotos819@gmail.com>
2025-09-19 16:44:52 +0300

    asda

--- server.js
+++ server.js
@@ -183,7 +183,7 @@ async function postCarousel(accessToken) {
   ];
   const payload = {
     media_type: 'PHOTO',
-    post_mode: 'MEDIA_UPLOAD', // or keep 'DIRECT_POST'
+    post_mode: 'DIRECT_POST', // or keep 'DIRECT_POST'
     post_info: {
       title: 'Test',
       description: `🎵 ${song.name}\n#ttphotos`,
